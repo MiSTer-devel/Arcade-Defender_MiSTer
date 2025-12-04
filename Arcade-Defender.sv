212ed1218a001a5e7bbe24c33e3cdc30d236eb33
jackyangantelope <jack@retroremake.co>
2025-12-04 10:57:46 +0800

    modify frame

--- Arcade-Defender.sv
+++ Arcade-Defender.sv
@@ -57,6 +57,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -190,6 +192,8 @@ assign LED_POWER = 0;
 assign BUTTONS = 0;
 assign FB_FORCE_BLANK = '0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 assign VGA_DISABLE = 0;
 
 wire [1:0] ar = status[17:16];
